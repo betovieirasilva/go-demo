eddb5486fb231beb9889023cf232bf59561981e7
gilberto_silva_tec <gilberto.silva@tecsinapse.com.br>
2021-10-14 15:53:43 -0300

    Exemplo de metodo / function / interface

--- basics/methods-interfaces/main.go
+++ basics/methods-interfaces/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"math"
+	"strconv"
 )
 
 type Vertex struct {
@@ -10,6 +12,10 @@ type Vertex struct {
 }
 
 type MyFloat float64
+
+type Abser interface {
+	Abs() float64
+}
 
 /**
 Go não tem classes. No entanto, você pode definir métodos em tipos.
@@ -20,6 +26,24 @@ Neste exemplo, o método Abs tem um receptor do tipo Vertex chamado v.
 //este é um método
 func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.Y + v.Y*v.Y)
+}
+
+func (v Vertex) ToString() string {
+	var b bytes.Buffer
+	b.WriteString("Vertex {\n  X:")
+	b.WriteString(strconv.FormatFloat(v.X, 'f', -1, 64))
+	b.WriteString("\n  Y:")
+	b.WriteString(strconv.FormatFloat(v.Y, 'f', -1, 64))
+	b.WriteString("\n}")
+	return b.String()
+}
+
+/* Ponteiros receptores pemitino a alteração
+interna dos valores da struct ao passar por parâmetros
+*/
+func (v *Vertex) SetValues(x float64, y float64) {
+	v.X = x
+	v.Y = y
 }
 
 //Aqui está Abs escrito como uma função regular, sem qualquer alteração na funcionalidade.
@@ -35,13 +59,41 @@ func (f MyFloat) Abs() float64 {
 	return float64(f)
 }
 
+func interfaceTest() {
+	fmt.Println("====== TESTE COM INTERFACE =====")
+
+	var a Abser
+	f := MyFloat(-math.Sqrt2)
+	v := Vertex{3, 4}
+
+	fmt.Println("F=> ", f.Abs())
+	fmt.Println("V=> ", v.ToString())
+	a = f
+	fmt.Println("A_F=> ", a.Abs())
+
+	a = &v
+	fmt.Println("A_&V=> ", a.Abs())
+
+	a = v //V não implementa Abs, apenas sua referência tem o método Abs
+	fmt.Println("A_V=> ", a.Abs())
+
+}
+
 func main() {
 	//Teste 1
-	v := Vertex{3, 4}
+	v := Vertex{3.5, 4.8}
 	fmt.Println(" Valor ABS ", v.Abs()) //Abs está associado a Vertex
 	fmt.Println(" Valor ABS ", Abs(v))  //Abs está associado a Vertex
 
 	//Teste 2
 	f := MyFloat(-math.Sqrt2)
 	fmt.Println(f.Abs())
+
+	//Teste3
+	fmt.Println("\nTeste A:\n", v.ToString())
+
+	v.SetValues(50.6, 89.5)
+	fmt.Println("\nTeste B:\n", v.ToString())
+
+	interfaceTest()
 }
