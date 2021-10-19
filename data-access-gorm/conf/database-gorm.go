1d24c5665722d005c05774df4e4634811f62f880
gilberto_silva_tec <gilberto.silva@tecsinapse.com.br>
2021-10-19 14:44:32 -0300

    Adicionado log para exibir todos os SQLs executados

--- data-access-gorm/conf/database-gorm.go
+++ data-access-gorm/conf/database-gorm.go
@@ -6,12 +6,15 @@ import (
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func SetupDatabaseGorm() *gorm.DB {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	dsn := "root:1@tcp(127.0.0.1:3306)/recordings?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info), //habilita o log para exibir todos os SQLs executados
+	})
 
 	if err != nil {
 		log.Fatal(err) //exit
